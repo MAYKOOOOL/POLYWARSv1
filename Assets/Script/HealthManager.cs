56c8035f6af48bce5c25de0397aa6fa6dd11cda9
MAYKOL <164203146+MAYKOOOOL@users.noreply.github.com>
2025-03-23 15:17:09 +0800

    HEALTH BAR

---
+++ Assets/Script/HealthManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthManager : MonoBehaviour
+{
+    public Image healthBar;
+    public float HealthAmount = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        HealthAmount -= damage;
+        healthBar.fillAmount = HealthAmount / 100f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+            TakeDamage(20);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Heal(20);
+        }
+    }
+    public void Heal(float healAmt)
+    {
+        HealthAmount += healAmt;
+        HealthAmount = Mathf.Clamp(HealthAmount, 0, 100);
+
+        healthBar.fillAmount = HealthAmount / 100f;
+    }
+}
+
+
